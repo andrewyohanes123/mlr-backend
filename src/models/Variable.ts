ed964b12650436611b9f445d21112bf003cbedc0
andrewyohanes123 <andrew.yohanes123@gmail.com>
2022-08-07 22:58:38 +0800

    (+) first step calculations

--- src/models/Variable.ts
+++ src/models/Variable.ts
@@ -3,6 +3,23 @@ import { SequelizeAttributes } from './typings/SequelizeAttributes';
 import { Factory } from './typings/ModelInterface';
 import ModelFactoryInterface from './typings/ModelFactoryInterface';
 
+export type TFirstStep = {
+	x1y: number;
+	x2y: number;
+	x3y: number;
+	x4y: number;
+	x1x2: number;
+	x1x3: number;
+	x1x4: number;
+	x2x3: number;
+	x2x4: number;
+	x3x4: number;
+	x12: number;
+	x22: number;
+	x32: number;
+	x42: number;
+};
+
 export interface VariableAttributes {
 	id?: number;
 	gender: string;
@@ -14,10 +31,12 @@ export interface VariableAttributes {
 	x4: number;
 	created_at?: Date;
 	updated_at?: Date;
+	first_step?: TFirstStep;
 }
 
-export interface VariableInstance extends Sequelize.Instance<VariableAttributes>, VariableAttributes {
-}
+export interface VariableInstance
+	extends Sequelize.Instance<VariableAttributes>,
+		VariableAttributes {}
 
 export interface Associate {
 	(models: ModelFactoryInterface): void;
@@ -29,12 +48,12 @@ export const VariableFactory: Factory<VariableInstance, VariableAttributes> = (
 ): Sequelize.Model<VariableInstance, VariableAttributes> => {
 	const attributes: SequelizeAttributes<VariableAttributes> = {
 		gender: {
-			type: DataTypes.ENUM(["Laki - Laki", "Perempuan"]),
+			type: DataTypes.ENUM(['Laki - Laki', 'Perempuan']),
 			allowNull: false,
 		},
 		occupation: {
 			type: DataTypes.STRING(191),
-			allowNull: false
+			allowNull: false,
 		},
 		age: {
 			type: DataTypes.INTEGER(4),
@@ -42,28 +61,55 @@ export const VariableFactory: Factory<VariableInstance, VariableAttributes> = (
 		},
 		x1: {
 			type: DataTypes.FLOAT,
-			allowNull: false
+			allowNull: false,
 		},
 		x2: {
 			type: DataTypes.FLOAT,
-			allowNull: false
+			allowNull: false,
 		},
 		x3: {
 			type: DataTypes.FLOAT,
-			allowNull: false
+			allowNull: false,
 		},
 		x4: {
 			type: DataTypes.FLOAT,
-			allowNull: false
+			allowNull: false,
 		},
 	};
 	const Variable: Sequelize.Model<VariableInstance, VariableAttributes> = sequelize.define<
 		VariableInstance,
 		VariableAttributes
-	>('variable', attributes, { underscored: true });
+	>('variable', attributes, {
+		underscored: true,
+		hooks: {
+			afterFind(instances, fn?) {
+				if (Array.isArray(instances)) {
+					instances.forEach(instance => {
+						const { x1, x2, x3, x4 } = instance;
+						const y = Math.fround((x1 + x2 + x3 + x4) / 4);
+						instance.setDataValue('first_step', {
+							x1y: x1 * y,
+							x2y: x2 * y,
+							x3y: x3 * y,
+							x4y: x4 * y,
+							x1x2: x1 * x2,
+							x1x3: x1 * x3,
+							x1x4: x1 * x4,
+							x2x3: x2 * x3,
+							x2x4: x2 * x4,
+							x3x4: x3 * x4,
+							x12: Math.pow(x1, 2),
+							x22: Math.pow(x2, 2),
+							x32: Math.pow(x3, 2),
+							x42: Math.pow(x4, 2),
+						});
+					});
+				}
+			},
+		},
+	});
 
-	Variable.associate = (models: ModelFactoryInterface): void => {
-	};
+	Variable.associate = (models: ModelFactoryInterface): void => {};
 
 	return Variable;
 };
--- src/routes/variables.ts
+++ src/routes/variables.ts
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import ModelFactoryInterface from '../models/typings/ModelFactoryInterface';
 import { Routes } from './typings/RouteInterface';
@@ -11,90 +10,104 @@ import NotFoundError from '../classes/NotFoundError';
 import { VariableInstance, VariableAttributes } from '../models/Variable';
 
 const variablesRoutes: Routes = (
-    app: express.Application,
-    models: ModelFactoryInterface,
+	app: express.Application,
+	models: ModelFactoryInterface,
 ): express.Router => {
-    const router: express.Router = express.Router();
+	const router: express.Router = express.Router();
 
-    router.get(
-        '/',
-        Parser.validateQ(),
-        a(
-            async (req: express.Request, res: express.Response): Promise<void> => {
-                const parsed: sequelize.FindOptions<VariableInstance> = Parser.parseQuery<VariableInstance>(
-                    req.query.q,
-                    models,
-                );
-                const data: PaginatedResult<VariableInstance> = await models.Variable.findAndCountAll(parsed);
-                const body: OkResponse = { data };
+	router.get(
+		'/',
+		Parser.validateQ(),
+		a(
+			async (req: express.Request, res: express.Response): Promise<void> => {
+				const parsed: sequelize.FindOptions<VariableInstance> = Parser.parseQuery<
+					VariableInstance
+				>(req.query.q, models);
+				const data: PaginatedResult<VariableInstance> = await models.Variable.findAndCountAll(
+					parsed,
+				);
+				const body: OkResponse = { data };
 
-                res.json(body);
-            },
-        ),
-    );
+				res.json(body);
+			},
+		),
+	);
 
-    router.get(
-        '/:id',
-        a(
-            async (req: express.Request, res: express.Response): Promise<void> => {
-                const { id }: any = req.params;
-                const variable: VariableInstance | null = await models.Variable.findByPk(id);
-                if (!variable) throw new NotFoundError('Variable tidak ditemukan');
-                const body: OkResponse = { data: variable };
+	router.get(
+		'/calculated-variables',
+		a(
+			async (req: express.Request, res: express.Response): Promise<void> => {
+				const data: PaginatedResult<VariableInstance> = await models.Variable.findAndCountAll({
+					attributes: ['x1', 'x2', 'x3', 'x4'],
+				});
+				const body: OkResponse = { data };
 
-                res.json(body);
-            },
-        ),
-    );
+				res.json(body);
+			},
+		),
+	);
 
-    router.post(
-        '/',
-        // validation,
-        a(
-            async (req: express.Request, res: express.Response): Promise<void> => {
-                const attributes: VariableAttributes = req.body;
-                const variable: VariableInstance = await models.Variable.create(attributes);
-                const body: OkResponse = { data: variable };
+	router.get(
+		'/:id',
+		a(
+			async (req: express.Request, res: express.Response): Promise<void> => {
+				const { id }: any = req.params;
+				const variable: VariableInstance | null = await models.Variable.findByPk(id);
+				if (!variable) throw new NotFoundError('Variable tidak ditemukan');
+				const body: OkResponse = { data: variable };
 
-                res.json(body);
-            },
-        ),
-    );
+				res.json(body);
+			},
+		),
+	);
 
-    router.put(
-        '/:id',
-        // validation,
-        a(
-            async (req: express.Request, res: express.Response): Promise<void> => {
-                const { id }: any = req.params;
-                const attributes: VariableAttributes = req.body;
-                const variable: VariableInstance | null = await models.Variable.findByPk(id);
-                if (!variable) throw new NotFoundError('Variable tidak ditemukan');
-                const updatedVariable: VariableInstance = await variable.update(attributes);
-                const body: OkResponse = { data: updatedVariable };
+	router.post(
+		'/',
+		// validation,
+		a(
+			async (req: express.Request, res: express.Response): Promise<void> => {
+				const attributes: VariableAttributes = req.body;
+				const variable: VariableInstance = await models.Variable.create(attributes);
+				const body: OkResponse = { data: variable };
 
-                res.json(body);
-            },
-        ),
-    );
+				res.json(body);
+			},
+		),
+	);
 
-    router.delete(
-        '/:id',
-        a(
-            async (req: express.Request, res: express.Response): Promise<void> => {
-                const { id }: any = req.params;
-                const variable: VariableInstance | null = await models.Variable.findByPk(id);
-                if (!variable) throw new NotFoundError('Variable tidak ditemukan');
-                await variable.destroy();
-                const body: OkResponse = { data: variable };
+	router.put(
+		'/:id',
+		// validation,
+		a(
+			async (req: express.Request, res: express.Response): Promise<void> => {
+				const { id }: any = req.params;
+				const attributes: VariableAttributes = req.body;
+				const variable: VariableInstance | null = await models.Variable.findByPk(id);
+				if (!variable) throw new NotFoundError('Variable tidak ditemukan');
+				const updatedVariable: VariableInstance = await variable.update(attributes);
+				const body: OkResponse = { data: updatedVariable };
 
-                res.json(body);
-            },
-        ),
-    );
+				res.json(body);
+			},
+		),
+	);
 
-    return router;
+	router.delete(
+		'/:id',
+		a(
+			async (req: express.Request, res: express.Response): Promise<void> => {
+				const { id }: any = req.params;
+				const variable: VariableInstance | null = await models.Variable.findByPk(id);
+				if (!variable) throw new NotFoundError('Variable tidak ditemukan');
+				await variable.destroy();
+				const body: OkResponse = { data: variable };
+
+				res.json(body);
+			},
+		),
+	);
+
+	return router;
 };
 
 export default variablesRoutes;
-    
